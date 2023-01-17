84717ee51d819ae6c663265892de15b5adbd8a24
nka2001 <89935053+nka2001@users.noreply.github.com>
2023-01-17 12:45:36 -0500

    DLL - Node Class Complete

--- src/main/java/com/mycompany/doublylinkedlist/Node.java
+++ src/main/java/com/mycompany/doublylinkedlist/Node.java
@@ -3,7 +3,26 @@ package com.mycompany.doublylinkedlist;
 /**
  *
  * @author nicka
+ * @param <T>
  */
-public class Node {
+public class Node<T> extends DoublyLinkedList {
+    
+    T data;
+    Node prev;
+    Node next;
+    
+    public Node(){
+        this.data = (T) "Default";
+        this.next = null;
+        this.prev = null;
+    }
+    
+    public Node(T data){
+        this.data = data;
+        this.next = null;
+        this.prev = null;
+        
+    }
+    
     
 }
